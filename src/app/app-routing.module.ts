04ec38fa97d18f5095f6a03bb71225b8ac1fe141
Sami Ullah <sami.ullah@cadt-solutions.com>
2019-03-18 14:48:33 +0400

    added search page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'details', loadChildren: './details/details.module#DetailsPageModule' },
+  { path: 'search', loadChildren: './search/search.module#SearchPageModule' },
 ];
 
 @NgModule({
