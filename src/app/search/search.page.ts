32a7b0183d90085ebcd90ab781a8720128be107e
Samiullah Khan <samiullah.khan.1989@gmail.com>
2019-03-25 22:51:57 +0400

    cleaned up search page

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { of, timer, Observable } from 'rxjs';
-import { map } from "rxjs/operators";
-import { LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
@@ -12,20 +9,11 @@ export class SearchPage implements OnInit {
   loader: any;
 
   constructor(
-    private loadingCtrl: LoadingController
   ) { }
 
   ngOnInit() {
-    this.presentLoading()
   }
 
-  async presentLoading () {
-    this.loader = await this.loadingCtrl.create({
-      message: 'Uploading Image, Please wait...',
-      spinner: 'dots'
-    });
-    await this.loader.present();
-  }
 
   getItems() {
 
