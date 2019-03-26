085ae0fd60030934ce760e7ec242f372146eb87a
Sami Ullah <sami.ullah@cadt-solutions.com>
2019-03-26 18:06:15 +0400

    closes #2

--- src/app/shared/stream.directive.ts
+++ src/app/shared/stream.directive.ts
@@ -7,25 +7,22 @@ import { Router } from '@angular/router';
   selector: '[app-stream]'
 })
 export class StreamDirective {
-
   subscriptions = new Subscription();
-
 
   constructor(private el: ElementRef, private router: Router) {
     const secretElement = this.el.nativeElement;
-    
 
     const clickStream = fromEvent(secretElement, 'click');
     this.subscriptions.add(clickStream
-    .pipe(
-      buffer(clickStream.pipe(throttleTime(600, asyncScheduler , {leading: false, trailing: true}))),
-      map(arr => arr.length),
-      filter(len => len === 4)
+      .pipe(
+        buffer(clickStream.pipe(throttleTime(600, asyncScheduler, { leading: false, trailing: true }))),
+        map(arr => arr.length),
+        filter(len => len === 4)
       )
-    .subscribe(resp => {
-      console.log('clicked', resp);
-      this.router.navigate(['/settings'])
-    }))
-   }
+      .subscribe(resp => {
+        console.log('clicked', resp);
+        this.router.navigate(['/settings'])
+      }))
+  }
 
 }
