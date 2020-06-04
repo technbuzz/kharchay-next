3bf6bd63bfe5abb1eaa17f63bec8e9590685bc4f
technbuzz <samiullah.khan.1989@gmail.com>
2020-06-05 01:43:11 +0400

    @angular/core migration - migration-v9-missing-injectable Missing @Injectable and incomplete provider definition migration. In Angular 9, enforcement of @Injectable decorators for DI is a bit stricter and incomplete provider definitions behave differently. Read more about this here: https://v9.angular.io/guide/migration-injectable

--- src/app/shared/hammerConfig.ts
+++ src/app/shared/hammerConfig.ts
@@ -1,5 +1,7 @@
 import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class MyHammerConfig extends HammerGestureConfig {
   // overrides = <any>{
   //   pan: { direction: Hammer.DIRECTION_ALL },
