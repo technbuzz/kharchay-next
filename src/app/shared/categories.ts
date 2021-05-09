65fad15db7d9c0f12a255f38ee44e2547a0f1030
technbuzz <samiullah.khan.1989@gmail.com>
2021-05-09 11:48:44 +0400

    added education category

--- src/app/shared/categories.ts
+++ src/app/shared/categories.ts
@@ -5,6 +5,7 @@ export const categories = [
   { title:'food', subCategory: ['breakfast','lunch','dinner','other']},
   { title:'grocery', subCategory: ['bread','dairy','fruits','general','vegetables','dry fruits','other']},
   { title:'health'},
+  { title:'Education'},
   { title:'snacks'},
   { title:'transport'},
   { title:'toiletry'},
