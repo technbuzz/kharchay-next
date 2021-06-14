cb30eba852562dcf1eec809485dd02eb4ba22ad0
Samiullah Khan <samiullah.khan.1989@gmail.com>
2021-06-14 21:43:23 +0400

    added chartist for now for simple chart

--- src/app/summary/pie/pie.ts
+++ src/app/summary/pie/pie.ts
@@ -1,8 +1,7 @@
-import { Component, Input, ChangeDetectionStrategy, ElementRef, NgZone, OnInit } from '@angular/core';
-import { ChartType } from 'chart.js';
+import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core';
 
-import { Chart } from 'frappe-charts/dist/frappe-charts.esm.js';
-import 'frappe-charts/dist/frappe-charts.min.css';
+declare var Chartist: any;
+
 @Component({
   selector: 'pie',
   templateUrl: 'pie.html',
@@ -15,37 +14,24 @@ export class PieComponent implements OnInit{
 
   public dataSet = {
     labels: ['a','b','c'],
-    datasets: [
-      {
-        values: [1,2,3]
-      }
-    ]
+    series: [1,2,3]
   };
-
-  // @Output()
-  // chartClicked = new EventEmitter();
-
-
-  public doughnutChartType: ChartType = 'doughnut';
-  donutOptions: any = {
-    legend: {
-      display: true,
-      position: 'right',
-      labels: {
-        // fontColor: 'rgb(255, 99, 132)'
-      }
-    },
-  };
-
 
   constructor() {}
 
   ngOnInit() {
     this.dataSet = {
       labels: this.doughnutChartLabels,
-      datasets: [ {
-        values: this.doughnutChartData
-      }]
+      series: this.doughnutChartData
     };
+
+    new Chartist.Pie('.ct-chart', this.dataSet, {
+      donut:true,
+      donutWidth: 60,
+      donutSolid: true,
+      startAngle: 270,
+      showLabel: true
+    });
+
   }
 }
