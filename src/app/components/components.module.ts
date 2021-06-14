2d50722ba2759658825368c12ecee2dbfb4cf2a7
Samiullah Khan <samiullah.khan.1989@gmail.com>
2021-06-14 21:46:23 +0400

    removed ng2-charts

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ExpenseItemComponent } from './expense-item/expense-item';
 import { IonicModule } from '@ionic/angular';
 import { ExpenseImageComponent } from './expense-image/expense-image';
-import { ChartsModule } from 'ng2-charts';
 import { RecurringComponent } from './recurring/recurring.component';
 
 
@@ -11,8 +10,7 @@ import { RecurringComponent } from './recurring/recurring.component';
   declarations: [ExpenseItemComponent, ExpenseImageComponent, RecurringComponent],
   imports: [
     CommonModule,
-    IonicModule,
-    ChartsModule
+    IonicModule
   ],
   exports: [ExpenseItemComponent, ExpenseImageComponent, RecurringComponent],
   entryComponents: []
