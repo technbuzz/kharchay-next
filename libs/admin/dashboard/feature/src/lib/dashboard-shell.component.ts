bc2b6fd28a889ae4ca1f8819f099b3e66ee88d6c
technbuzz <samiullah.khan.1989@gmail.com>
2023-05-03 21:44:10 +0400

    fixed some grid styling

--- libs/admin/dashboard/feature/src/lib/dashboard-shell.component.ts
+++ libs/admin/dashboard/feature/src/lib/dashboard-shell.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'kh-dashboard-shell',
   template: `
-  <div class="wrapper grid grid-cols-3">
+  <div class="wrapper grid grid-cols-3 grid-rows-2 h-full">
     <kh-transactions-mini class="col-span-2"></kh-transactions-mini>
   </div>
 `,
   styles: [`
-    :host { display: block; padding: 1rem; }
+    :host { @apply block h-full p-1 }
 `],
 })
 export class DashboardShellComponent {}
