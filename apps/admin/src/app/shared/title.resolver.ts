3209c4faaa8293532050e4e7f8d574bb3fd095fe
technbuzz <samiullah.khan.1989@gmail.com>
2023-10-16 16:48:14 +0400

    update: nx to 16.10

--- apps/admin/src/app/shared/title.resolver.ts
+++ apps/admin/src/app/shared/title.resolver.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Title } from '@angular/platform-browser';
 import { GeneralService } from './general.service';
 
 @Injectable({ providedIn: 'root' })
-export class TitleResolver implements Resolve<any> {
+export class TitleResolver  {
   constructor(private title: Title, private gs: GeneralService){}
   resolve(route: ActivatedRouteSnapshot): Observable<any> | Promise<any> | any {
     const newTitle = route.data['title']
