6ce28b8b379289c1adf98d4dc040902a62d046c3
technbuzz <samiullah.khan.1989@gmail.com>
2024-01-19 10:57:16 +0400

    add sub cat selection

--- libs/mobile/create/ui/src/lib/newx/newx.component.ts
+++ libs/mobile/create/ui/src/lib/newx/newx.component.ts
@@ -1,81 +1,54 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, ViewChild, ElementRef, Input, Output, EventEmitter } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
 import { IonGrid, IonInput, IonModal, IonicModule } from '@ionic/angular';
 import {format} from 'date-fns/format'
+import { Category, categories } from './categories';
+
 
 @Component({
   selector: 'kh-newx',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,DatePipe, ReactiveFormsModule],
   templateUrl: './newx.component.html',
   styleUrl: './newx.component.css'
 })
 export class NewxComponent {
-  selectedCategory = { label: 'Grocery', icon: 'cart-outline' }
+  selectedCategory: Category = { title: 'Grocery', icon: 'cart-outline' }
 
   @ViewChild(IonModal) modal!: IonModal;
   @ViewChild('expenseDate') dateEl!: ElementRef<HTMLInputElement>;
+
+  @Input() parent!: UntypedFormGroup;
+  @Output() onSubmit = new EventEmitter()
 
   ngAfterViewInit() {
     this.dateEl.nativeElement.value = format(new Date('2024-01-11'), 'yyyy-MM-dd')
   }
 
+  categories = categories
+
   onSelect(e:any) {
+
     console.log(e)
 
   }
 
   date = format(new Date('2024-01-11'), 'yyyy-MM-dd');
-  categories = [
-    {
-      label: 'Bills',
-      icon: 'receipt-outline',
-    },
-    {
-      label: 'Clothes',
-      icon: 'shirt-outline',
-    },
-    {
-      label: 'Entertainment',
-      icon: 'bowling-ball-outline'
-    },
-    {
-      label: 'Food',
-      icon: 'restaurant-outline'
-    },
-    {
-      label: 'Grocery',
-      icon: 'cart-outline'
-    },
-    {
-      label: 'Health',
-      icon: 'medkit-outline'
-    },
-    {
-      label: 'Education',
-      icon: 'school-outline'
-    },
-    {
-      label: 'Snacks',
-      icon: 'fast-food-outline'
-    },
-    {
-      label: 'Transport',
-      icon: 'train-outline'
-    },
-    {
-      label: 'Toiletry',
-      icon: 'cut-outline'
-    },
-    {
-      label: 'Other',
-      icon: 'ellipsis-horizontal-outline'
-    },
 
-  ]
+  selecteCategory(category: Category) {
 
-  selecteCategory(category: any) {
-    this.selectedCategory = category
-    this.modal.dismiss()
+    if(category.categories) {
+      this.categories = category.categories
+
+    } else {
+      this.selectedCategory = category
+      this.parent.controls['category'].setValue({ title: category.title })
+      this.categories = categories
+      console.log(this.parent.value)
+      this.modal.dismiss()
+
+    }
   }
 
 }
