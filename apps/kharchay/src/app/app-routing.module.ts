54481a95db824561afa96adc239ad5c65de6ccbf
technbuzz <samiullah.khan.1989@gmail.com>
2024-01-20 11:06:59 +0400

    added the feature of back button

--- apps/kharchay/src/app/app-routing.module.ts
+++ apps/kharchay/src/app/app-routing.module.ts
@@ -13,7 +13,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  imports: [
+
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
