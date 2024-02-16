8d575fed6c20ff7ed4cf280806fed98c5d4749bb
technbuzz <samiullah.khan.1989@gmail.com>
2024-02-16 19:06:25 +0400

    all modules removed

--- apps/kharchay/src/app/app-routing.module.ts
+++ apps/kharchay/src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadComponent: () => import('./home/home.page').then(m => m.HomePage) },
-  { path: 'new', loadChildren: () => import('@kh/mobile/create/feature').then(m => m.MobileCreateFeatureModule) },
-  { path: 'details', loadChildren: () => import('./details/details.module').then(m => m.DetailsPageModule) },
-  { path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchPageModule) },
+  { path: 'new', loadComponent: () => import('@kh/mobile/create/feature').then(m => m.CreateComponent) },
+  { path: 'details', loadComponent: () => import('./details/details.page').then(m => m.DetailsPage) },
+  { path: 'search', loadComponent: () => import('./search/search.page').then(m => m.SearchPage) },
   { path: 'filter', loadComponent: () => import('./filter/filter.page').then(m => m.FilterPage) },
   { path: 'summary', loadComponent: () => import('./summary/summary.page').then(m => m.SummaryPage) },
   { path: 'settings', loadComponent: () => import('./settings/settings.page').then(m => m.SettingsPage) },
@@ -14,7 +14,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
--- libs/mobile/create/feature/src/index.ts
+++ libs/mobile/create/feature/src/index.ts
@@ -1,2 +1,1 @@
-export * from './lib/mobile-create-feature.module';
 export * from './lib/create/create.component'
