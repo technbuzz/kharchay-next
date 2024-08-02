84591853981cf1a1526a1d9bdaf31841051935b3
technbuzz <samiullah.khan.1989@gmail.com>
2024-08-02 18:26:56 +0400

    chore: lib fix

--- libs/common/api-interface/src/index.ts
+++ libs/common/api-interface/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/common-api-interface';
+export * from './lib/categories'
--- libs/mobile/create/data-access/src/index.ts
+++ libs/mobile/create/data-access/src/index.ts
@@ -1,3 +1,1 @@
-export * from './lib/mobile-create-data-access.module';
-export * from './lib/categories'
 export * from './lib/create/create.service'
