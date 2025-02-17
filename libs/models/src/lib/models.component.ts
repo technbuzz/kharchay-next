75f4505b4d7955d99b7873a6dbeef2b18ae14c56
technbuzz <samiullah.khan.1989@gmail.com>
2025-02-17 13:28:27 +0400

    added models lib

---
+++ libs/models/src/lib/models.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-models',
+  imports: [],
+  template: `
+    <p>
+      models works!
+    </p>
+  `,
+  styles: ``
+})
+export class ModelsComponent {
+
+}
---
+++ libs/models/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of models
+ */
+
+export * from './lib/models.service';
+export * from './lib/models.component';
