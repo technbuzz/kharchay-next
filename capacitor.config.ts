801aabe2ce31f86a83c8d41f38dc1014352f7ef8
technbuzz <samiullah.khan.1989@gmail.com>
2025-03-21 21:13:20 +0400

    added capactior after nx less

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.app.kharchay',
+  appName: 'kharchay',
+  webDir: 'dist/kharchay/browser',
+};
+
+export default config;
